afc358912dba14b8b4f2ce754bb2028bb76b3aeb
libus1204 <libus1204@naver.com>
2019-02-07 17:03:51 +0900

    Merge branch 'master' of https://github.com/libus1204/bigdata2019
    # Conflicts:
    #	01. Jump to python/chap00_pycharm_test/Korean_test.py
    ..

--- 01. Jump to python/chap04/chap04_review_1.py
+++ 01. Jump to python/chap04/chap04_review_1.py
@@ -38,3 +38,94 @@ def say():
 
 say()
 '''
+'''
+# p.147 여러 개의 입력값을 받는 함수 만들기
+def sum_mul(choice, *args):
+    if choice == "sum":
+        result = 0
+        for i in args:
+            result += i
+    elif choice == "sub":
+        result = 0
+        for i in args:
+            result -= i
+    elif choice == "mul":
+        result = 1
+        for i in args:
+            result *= i
+    elif choice == "div":
+        result = 100
+        for i in args:
+            result /= i
+    return result
+
+result = sum_mul("sum", 1,2,3,4,5)
+print(result)
+
+result = sum_mul("sub", 1,2,3,4,5)
+print(result)
+
+result = sum_mul('mul', 1,2,3,4,5)
+print(result)
+
+result = sum_mul('div', 1,2,2,5)
+print(result)
+'''
+'''
+# p.149 함수 결과값은 언제나 하나
+def sum_and_mul(a,b):
+    return a+b, a*b
+result = sum_and_mul(3,4)
+sum, mul = sum_and_mul(3,4)
+print(result)
+print(sum)
+print(mul)
+
+def say_nick(nick):
+    if nick == "바보":
+        return
+    print("나의 별명은 %s 입니다." % nick)
+
+say_nick('헤헤')
+say_nick('ㅎㅎ')
+say_nick('바보')
+'''
+'''
+# p.152 입력 인수에 초깃값 미리 설정하기
+def say_myself(name, old, man=True):
+    print("나의 이름은 %s 입니다." % name)
+    print("나이는 %s살 입니다." % old)
+    if man: print("남자입니다.")
+    else: print("여자입니다.")
+
+say_myself("김상민", 30, True)
+say_myself('김상민', 30)
+say_myself('김상미', 30, False)
+'''
+'''
+# p.154 함수 안에서 선언된 변수의 효력 범위
+a = 3
+def vartest(a):
+    a += 1
+    return a
+print(a) # 전역변수가 3이니까 3출력
+a = vartest(a)
+print(a) # vartest 함수에서 +1 이 되고 return 됐으므로 4 출력
+
+def vartest2():
+    global a
+    a *= 2
+
+print(a)
+vartest2()
+print(a)
+'''
+# p.157 input 사용
+#number = input("숫자를 입력하세요 : ")
+#print(number)
+print("life" "is" "too short")
+print("life "+"is "+"too short!")
+print("life", "is", "too short", "!")
+for i in range(10):
+    print(i, end=' ')
+
