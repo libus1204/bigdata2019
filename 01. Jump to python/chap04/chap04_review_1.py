72e28c15e49a05955b3f48eb9086021454970aa0
libus1204 <libus1204@naver.com>
2019-02-07 16:37:30 +0900

    Merge branch 'master' of https://github.com/libus1204/bigdata2019
    # Conflicts:
    #	01. Jump to python/chap00_pycharm_test/Korean_test.py
    ..

---
+++ 01. Jump to python/chap04/chap04_review_1.py
@@ -0,0 +1,40 @@
+'''
+p.143
+def sum(a,b):
+    return a+b
+
+d=3
+e=4
+c=sum(d,e)
+print(c)
+'''
+'''
+# p.144 일반적함수
+def sum(a,b):
+    result = a+b
+    return result
+
+a=sum(6,4)
+print(a)
+'''
+'''
+# p.144 입력값이 없는 함수
+def say():
+    return 'Hi'
+a = say()
+print(a)
+'''
+'''
+# p.145 결과값이 없는 함수
+def sum(a,b):
+    print("%d, %d 의 합은 %d 입니다." %(a, b, a+b))
+
+sum(5,6)
+'''
+'''
+# p.146 입력값도 결과값도 없는 함수
+def say():
+    print('Hi')
+
+say()
+'''
