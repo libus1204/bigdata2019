efc475ff58d8a42a10e945b71b4a172d6c8c010e
libus1204 <libus1204@naver.com>
2019-02-10 16:39:48 +0900

    집에서 카페 함수 문제풀이 2019_02_10_16_39

--- 01. Jump to python/chap04/greet_user.py
+++ 01. Jump to python/chap04/greet_user.py
@@ -2,10 +2,10 @@ import sys
 usernames = sys.argv[1:]
 
 def greet_users(usernames):
-     for i in usernames:
-          print("Hello, ",end=' ')
-          str_usernames=str(i)
-          print(str_usernames[0].upper()+str_usernames[1:],end='')
+     for username in usernames:
+          print("Hello, ", end=' ')
+          str_usernames=str(username)
+          print(str_usernames[0].upper()+str_usernames[1:], end='')
           print("!")
 
 greet_users(usernames)
