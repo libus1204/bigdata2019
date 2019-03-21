450d497c60aea18ceb97028bcd0f90dd55e95ffd
libus1204 <libus1204@naver.com>
2019-03-21 15:10:34 +0900

    2019_03_21_15_10

---
+++ 03. AI/01. Machine Learning/192_mushroom-download.py
@@ -0,0 +1,9 @@
+import urllib.request as req
+local= "mushroom.csv"
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data"
+req.urlretrieve(url, local)
+print("ok")
+
+# https://archive.ics.uci.edu/ml/machine-learning-databases/
+
+# https://archive.ics.uci.edu/ml/index.php
